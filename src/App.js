bc573bd0f3dac50fc7dcf9006b667017f90d7aa5
hridoy-orangetoolz <hridoy@orangetoolz.com>
2021-09-28 14:02:49 +0600

    Form Handeling Done

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import MyList from "./components/list/Parent";
 import Practice from "./components/practice/ParentComponent";
 import Todo from "./components/todo/Parent";
 import RandomNumber from "./components/random/RandomNumber";
+import StripeCheck from "./components/stripe/Stripe";
+import TodoS from "./components/todo/Todo";
+import Form from "./components/form/Form";
 
 function App() {
   return (
@@ -21,7 +24,10 @@ function App() {
         <Route exact path="/mylist" component={MyList} />
         <Route exact path="/practice" component={Practice} />
         <Route exact path="/todo" component={Todo} />
+        <Route exact path="/todos" component={TodoS} />
         <Route exact path="/random" component={RandomNumber} />
+        <Route exact path="/stripe" component={StripeCheck} />
+        <Route exact path="/form" component={Form} />
       </Switch>
     </Router>
   );
